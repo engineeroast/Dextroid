751b81f5fe4438b061ceeff4e824effa859c2f4d
Rajat <rajat.priyam@gmail.com>
2021-01-20 20:57:45 +0530

    pix

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
 (function($) {
   "use strict";
 
@@ -223,3 +224,11 @@
   });
 
 })(jQuery);
+$('html').click(function (e) {
+    if (e.target.id == 'myPopup1') {
+        //do something
+    } else {
+        //do something
+    }
+});
+
